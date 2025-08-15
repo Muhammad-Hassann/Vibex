7eb8661b564b8c2a6b083be9e81d419bf7a317ac
Muhammad-Hassann <muhammadhassan202005@gmail.com>
2025-08-15 20:44:43 +0500

    Intialize project

---
+++ src/db/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+       const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`Database connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection error", error);
+        process.exit(1);
+    }
+};
